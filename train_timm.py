c49714222ae7f0c485140ba41ef7c6e42aac15da
DogWY <dogwangyan@163.com>
2025-02-11 22:09:27 +0800

    调整预处理方法

--- train_timm.py
+++ train_timm.py
@@ -8,70 +8,75 @@ from torch.utils.tensorboard import SummaryWriter
 
 from tqdm import trange
 
-batch_size = 32
-lr = 3e-5
-epochs = 300
-device = "cuda:0"
+if __name__ == "__main__":
+    batch_size = 32
+    lr = 3e-5
+    epochs = 300
+    device = "cuda:0"
 
-transform = transforms.Compose(
-    [
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-    ]
-)
+    transform = transforms.Compose(
+        [
+            transforms.Resize((224, 224)),
+            # 归一化处理
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
 
-full_dataset = OxfordIIITPet(
-    root="./data", split="trainval", transform=transform, download=True
-)
+    full_dataset = OxfordIIITPet(
+        root="./data", split="trainval", transform=transform, download=True
+    )
 
-# 按 80% 训练，20% 验证拆分
-train_size = int(0.8 * len(full_dataset))
-val_size = len(full_dataset) - train_size
-train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
+    # 按 80% 训练，20% 验证拆分
+    train_size = int(0.8 * len(full_dataset))
+    val_size = len(full_dataset) - train_size
+    train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
 
-# 创建 DataLoader
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    # 创建 DataLoader
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, num_workers=4
+    )
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
-model = timm.create_model("vit_base_patch16_224", pretrained=False)
+    model = timm.create_model("vit_base_patch16_224", pretrained=False)
 
-model = model.to(device)
+    model = model.to(device)
 
-criterion = torch.nn.CrossEntropyLoss()
+    criterion = torch.nn.CrossEntropyLoss()
 
-optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-writer = SummaryWriter()
+    writer = SummaryWriter()
 
-pbar = trange(epochs)
-for epoch in pbar:
-    model.train()
-    for i, (x, y) in enumerate(train_loader):
-        x, y = x.to(device), y.to(device)
-
-        optimizer.zero_grad()
-        output = model(x)
-        loss = criterion(output, y)
-        loss.backward()
-        optimizer.step()
-
-        writer.add_scalars(
-            "Loss", {"train": loss.item()}, epoch * len(train_loader) + i
-        )
-
-    model.eval()
-    with torch.no_grad():
-        total, correct = 0, 0
-        for x, y in val_loader:
+    pbar = trange(epochs)
+    for epoch in pbar:
+        model.train()
+        for i, (x, y) in enumerate(train_loader):
             x, y = x.to(device), y.to(device)
 
+            optimizer.zero_grad()
             output = model(x)
-            _, predicted = torch.max(output, 1)
-            total += y.size(0)
-            correct += (predicted == y).sum().item()
+            loss = criterion(output, y)
+            loss.backward()
+            optimizer.step()
 
-        writer.add_scalars(
-            "Accuracy", {"val": correct / total}, epoch * len(train_loader) + i
-        )
+            writer.add_scalars(
+                "Loss", {"train": loss.item()}, epoch * len(train_loader) + i
+            )
 
-    pbar.set_description(f"Epoch {epoch}, val acc: {correct / total}")
+        model.eval()
+        with torch.no_grad():
+            total, correct = 0, 0
+            for x, y in val_loader:
+                x, y = x.to(device), y.to(device)
+
+                output = model(x)
+                _, predicted = torch.max(output, 1)
+                total += y.size(0)
+                correct += (predicted == y).sum().item()
+
+            writer.add_scalars(
+                "Accuracy", {"val": correct / total}, epoch * len(train_loader) + i
+            )
+
+        pbar.set_description(f"Epoch {epoch}, val acc: {correct / total}")
